4c402cb7fc7d4faacb7767990541d72ea2e6a9bc
halleycaplan <hcaplan@lsoc.org>
2022-09-14 10:45:06 -0500

    Deleted some ifs

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -11,16 +11,13 @@ class GildedRose(object):
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
+                elif item.quality < 50:
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+                
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
