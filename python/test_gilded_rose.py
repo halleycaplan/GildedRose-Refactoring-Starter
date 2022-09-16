49ccbb393814e5e6940cb947180fd61caed0cbdd
halleycaplan <hcaplan@lsoc.org>
2022-09-16 09:02:13 -0500

    Added tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,27 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_Brie(self):
+        items = [Item("Aged Brie", 1, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals(21, items[0].quality)
+    def test_Sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 2, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(20, items[0].quality)
+    def test_Elixir(self):
+        items = [Item("Elixir of the Mongoose", 1, 7)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(6, items[0].quality)
+    def test_BackStage(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 2, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(21, items[0].quality)
+
 
 
 
