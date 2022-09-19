c3118cc3b12d273bb460907a8fd9a1b958c2dc2d
halleycaplan <hcaplan@lsoc.org>
2022-09-18 23:42:27 -0500

    Cleaned up code a bit; regular/conjured broken

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,16 +7,7 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
-                if item.quality > 0:
-                    item.quality = item.quality - 1
-                    item.sell_in = item.sell_in - 1
-                elif item.quality < 50:
-                    if item.sell_in < 11:
-                        item.quality = item.quality + 1
-                      
-            
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if 10 >= item.sell_in > 5:
                     item.quality = item.quality + 2
                 elif 5 >= item.sell_in > 0:
@@ -25,8 +16,6 @@ class GildedRose(object):
                     item.quality = 0
                 else:
                     item.quality = item.quality + 1
-                if item.quality > 50:
-                    item.quality = 50
                 item.sell_in = item.sell_in - 1
             
             elif item.name == "Aged Brie":
@@ -34,6 +23,28 @@ class GildedRose(object):
                     item.quality = item.quality + 1
                 if item.sell_in < 0:
                     item.quality = item.quality - item.quality
+            
+            elif item.name == "Conjured Mana Cake":
+                if item.quality > 2:
+                    item.quality = item.quality - 2
+                    item.sell_in = item.sell_in - 1
+                elif item.quality < 50:
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+                else:
+                    item.quality = 0
+ 
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                item.quality = 80
+
+            else:
+                if item.quality > 0:
+                    item.quality = item.quality - 1
+                    item.sell_in = item.sell_in - 1
+                elif item.quality < 50:
+                    if item.sell_in < 11:
+                        item.quality = item.quality + 1
+
 
         
                 
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -11,21 +11,25 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals(21, items[0].quality)
     def test_Sulfuras(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 2, 20)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 2, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(20, items[0].quality)
+        self.assertEquals(80, items[0].quality)
     def test_Elixir(self):
-        items = [Item("Elixir of the Mongoose", 1, 7)]
+        items = [Item("Elixir of the Mongoose", 1, 45)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(6, items[0].quality)
+        self.assertEquals(44, items[0].quality)
     def test_BackStage(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals(0, items[0].quality)
-
+    def test_Conjured(self):
+        items = [Item("Conjured Mana Cake", 1, 8)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals(6, items[0].quality)
 
 
 
